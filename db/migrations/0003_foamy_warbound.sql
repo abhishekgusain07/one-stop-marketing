222631c8fe7a0cf738454d215bfdbe2597190073
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-02-10 14:27:10 +0530

    init

---
+++ db/migrations/0003_foamy_warbound.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "invoices" ALTER COLUMN "id" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "subscriptions_plans" ALTER COLUMN "id" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "id" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "id" SET DATA TYPE text;
