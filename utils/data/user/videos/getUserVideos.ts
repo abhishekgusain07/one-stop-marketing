4bd3dbf071a4512936047c74cca7e5adb4f800f8
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-03-08 23:36:56 +0530

    getUserVideos

--- utils/data/user/videos/getUserVideos.ts
+++ utils/data/user/videos/getUserVideos.ts
@@ -1,6 +1,28 @@
-
 "use server"
 
-exportm :const getUserVideos {
+import { db } from "@/db/drizzle";
+import { products, users, videos } from "@/db/schema";
+import { NewProduct, Product, User, userCreateProps, Videos } from "@/utils/types";
+import { auth } from "@clerk/nextjs/server";
+import { uid } from "uid";
+import { eq } from "drizzle-orm"
 
+export const getUserVideos = async() => {
+    try{
+        const { userId } = await auth();
+        if(!userId) {
+            throw new Error("Unauthorized");
+        }
+        const user = await db.select().from(users).where(eq(users.userId, userId)).limit(1);
+
+        if(!user[0]) {
+            throw new Error("User not found");
+        }
+
+        const allVideos: Videos[] = await db.select().from(videos).where(eq(videos.userId, user[0].id));
+        return allVideos;
+    }catch(error) {
+        console.log("error ", error);
+        throw error;
+    }
 }
